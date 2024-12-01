85b431f5ec9dcb37ca19d3b9a86e134d985059c2
sqrtxander <xanderjames128@gmail.com>
2024-12-01 15:58:02 +1100

    day09 complete

---
+++ day09/part2/solve.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"aoc2019/intcode"
+	"aoc2019/utils"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+func solve(s string) int {
+	s = strings.TrimSpace(s)
+	program := utils.Map(strings.Split(s, ","), utils.HandledAtoi)
+
+	pc := intcode.NewIntcodeComputer(program)
+	pc.AddInputs(2)
+	return pc.Execute()
+}
+
+func main() {
+	var inputPath string
+	if len(os.Args) > 1 {
+		inputPath = os.Args[1]
+	} else {
+		_, currentFilePath, _, _ := runtime.Caller(0)
+		dir := filepath.Dir(currentFilePath)
+		dir = filepath.Dir(dir)
+		inputPath = filepath.Join(dir, "input.in")
+	}
+	contents, err := os.ReadFile(inputPath)
+	if err != nil {
+		log.Fatalf("Error reading file %s:\n%v\n", inputPath, err)
+		return
+	}
+	fmt.Println(solve(string(contents)))
+}
--- intcode/intcode.go
+++ intcode/intcode.go
@@ -5,101 +5,125 @@ import (
 )
 
 type IntcodeComputer struct {
-	IP     int
-	Memory []int
-	Inputs []int
-	Halted bool
+	IP      int
+	relBase int
+	Memory  map[int]int
+	Inputs  []int
+	Halted  bool
 }
 
-func getValFromMode(val int, program []int, mode int) int {
+func (pc *IntcodeComputer) getValFromMode(val int, mode int) int {
 	switch mode {
 	case 0:
-		return program[val]
+		return pc.Memory[val]
 	case 1:
 		return val
+	case 2:
+		return pc.Memory[val+pc.relBase]
 	default:
 		log.Fatalf("Invalid parameter mode: %d\n", mode)
 	}
 	return -1
 }
 
-func storeValFromMode(idx int, val int, program []int, mode int) {
+func (pc *IntcodeComputer) storeValFromMode(idx int, val int, mode int) {
+	var setIdx int
 	switch mode {
 	case 0:
-		program[idx] = val
+		setIdx = idx
+	case 2:
+		setIdx = idx + pc.relBase
 	default:
 		log.Fatalf("Invalid parameter mode for storing: %d\n", mode)
 	}
+	if setIdx < 0 {
+		log.Fatalf("Invalid memory address for storing: %d\n", setIdx)
+	}
+	pc.Memory[setIdx] = val
 }
 
-func add(ip int, program []int, p1Mode int, p2Mode int, p3Mode int) int {
-	num1 := getValFromMode(program[ip+1], program, p1Mode)
-	num2 := getValFromMode(program[ip+2], program, p2Mode)
-	storeValFromMode(program[ip+3], num1+num2, program, p3Mode)
-	return 4
+func (pc *IntcodeComputer) add(p1Mode int, p2Mode int, p3Mode int) {
+	num1 := pc.getValFromMode(pc.Memory[pc.IP+1], p1Mode)
+	num2 := pc.getValFromMode(pc.Memory[pc.IP+2], p2Mode)
+	pc.storeValFromMode(pc.Memory[pc.IP+3], num1+num2, p3Mode)
+	pc.IP += 4
 }
 
-func mult(ip int, program []int, p1Mode int, p2Mode int, p3Mode int) int {
-	num1 := getValFromMode(program[ip+1], program, p1Mode)
-	num2 := getValFromMode(program[ip+2], program, p2Mode)
-	storeValFromMode(program[ip+3], num1*num2, program, p3Mode)
-	return 4
+func (pc *IntcodeComputer) mult(p1Mode int, p2Mode int, p3Mode int) {
+	num1 := pc.getValFromMode(pc.Memory[pc.IP+1], p1Mode)
+	num2 := pc.getValFromMode(pc.Memory[pc.IP+2], p2Mode)
+	pc.storeValFromMode(pc.Memory[pc.IP+3], num1*num2, p3Mode)
+	pc.IP += 4
 }
 
-func input(ip int, program []int, inp int, p1Mode int) int {
-	storeValFromMode(program[ip+1], inp, program, p1Mode)
-	return 2
+func (pc *IntcodeComputer) input(p1Mode int) {
+	pc.storeValFromMode(pc.Memory[pc.IP+1], pc.Inputs[0], p1Mode)
+	pc.Inputs = pc.Inputs[1:]
+	pc.IP += 2
 }
 
-func output(ip int, program []int, p1Mode int) (int, int) {
-	out := getValFromMode(program[ip+1], program, p1Mode)
-	return 2, out
+func (pc *IntcodeComputer) output(p1Mode int) int {
+	out := pc.getValFromMode(pc.Memory[pc.IP+1], p1Mode)
+	pc.IP += 2
+	return out
 }
 
-func jnz(ip int, program []int, p1Mode int, p2Mode int) int {
-	p1 := getValFromMode(program[ip+1], program, p1Mode)
+func (pc *IntcodeComputer) jnz(p1Mode int, p2Mode int) {
+	p1 := pc.getValFromMode(pc.Memory[pc.IP+1], p1Mode)
 	if p1 != 0 {
-		return getValFromMode(program[ip+2], program, p2Mode) - ip
+		pc.IP = pc.getValFromMode(pc.Memory[pc.IP+2], p2Mode)
+	} else {
+		pc.IP += 3
 	}
-	return 3
 }
 
-func jz(ip int, program []int, p1Mode int, p2Mode int) int {
-	p1 := getValFromMode(program[ip+1], program, p1Mode)
+func (pc *IntcodeComputer) jz(p1Mode int, p2Mode int) {
+	p1 := pc.getValFromMode(pc.Memory[pc.IP+1], p1Mode)
 	if p1 == 0 {
-		return getValFromMode(program[ip+2], program, p2Mode) - ip
+		pc.IP = pc.getValFromMode(pc.Memory[pc.IP+2], p2Mode)
+	} else {
+		pc.IP += 3
 	}
-	return 3
 }
 
-func less(ip int, program []int, p1Mode int, p2Mode int, p3Mode int) int {
-	p1 := getValFromMode(program[ip+1], program, p1Mode)
-	p2 := getValFromMode(program[ip+2], program, p2Mode)
+func (pc *IntcodeComputer) less(p1Mode int, p2Mode int, p3Mode int) {
+	p1 := pc.getValFromMode(pc.Memory[pc.IP+1], p1Mode)
+	p2 := pc.getValFromMode(pc.Memory[pc.IP+2], p2Mode)
 	val := 0
 	if p1 < p2 {
 		val = 1
 	}
-	storeValFromMode(program[ip+3], val, program, p3Mode)
-	return 4
+	pc.storeValFromMode(pc.Memory[pc.IP+3], val, p3Mode)
+	pc.IP += 4
 }
 
-func equal(ip int, program []int, p1Mode int, p2Mode int, p3Mode int) int {
-	p1 := getValFromMode(program[ip+1], program, p1Mode)
-	p2 := getValFromMode(program[ip+2], program, p2Mode)
+func (pc *IntcodeComputer) equal(p1Mode int, p2Mode int, p3Mode int) {
+	p1 := pc.getValFromMode(pc.Memory[pc.IP+1], p1Mode)
+	p2 := pc.getValFromMode(pc.Memory[pc.IP+2], p2Mode)
 	val := 0
 	if p1 == p2 {
 		val = 1
 	}
-	storeValFromMode(program[ip+3], val, program, p3Mode)
-	return 4
+	pc.storeValFromMode(pc.Memory[pc.IP+3], val, p3Mode)
+	pc.IP += 4
+}
+
+func (pc *IntcodeComputer) adjRelBase(p1Mode int) {
+	pc.relBase += pc.getValFromMode(pc.Memory[pc.IP+1], p1Mode)
+    pc.IP += 2
 }
 
 func NewIntcodeComputer(memory []int) IntcodeComputer {
+	memoryMap := map[int]int{}
+	for i, num := range memory {
+		memoryMap[i] = num
+	}
 	return IntcodeComputer{
-		IP:     0,
-		Memory: memory,
-		Inputs: []int{},
-		Halted: false,
+		IP:      0,
+		relBase: 0,
+		Memory:  memoryMap,
+		Inputs:  []int{},
+		Halted:  false,
 	}
 }
 
@@ -136,24 +160,23 @@ func (pc *IntcodeComputer) Execute() int {
 		p3Mode := code % 10
 		switch opcode {
 		case 1:
-			pc.IP += add(pc.IP, pc.Memory, p1Mode, p2Mode, p3Mode)
+			pc.add(p1Mode, p2Mode, p3Mode)
 		case 2:
-			pc.IP += mult(pc.IP, pc.Memory, p1Mode, p2Mode, p3Mode)
+			pc.mult(p1Mode, p2Mode, p3Mode)
 		case 3:
-			pc.IP += input(pc.IP, pc.Memory, pc.Inputs[0], p1Mode)
-			pc.Inputs = pc.Inputs[1:]
+			pc.input(p1Mode)
 		case 4:
-			dip, outVal := output(pc.IP, pc.Memory, p1Mode)
-			pc.IP += dip
-			return outVal
+			return pc.output(p1Mode)
 		case 5:
-			pc.IP += jnz(pc.IP, pc.Memory, p1Mode, p2Mode)
+			pc.jnz(p1Mode, p2Mode)
 		case 6:
-			pc.IP += jz(pc.IP, pc.Memory, p1Mode, p2Mode)
+			pc.jz(p1Mode, p2Mode)
 		case 7:
-			pc.IP += less(pc.IP, pc.Memory, p1Mode, p2Mode, p3Mode)
+			pc.less(p1Mode, p2Mode, p3Mode)
 		case 8:
-			pc.IP += equal(pc.IP, pc.Memory, p1Mode, p2Mode, p3Mode)
+			pc.equal(p1Mode, p2Mode, p3Mode)
+        case 9:
+            pc.adjRelBase(p1Mode)
 		case 99:
 			pc.Halted = true
 			break
